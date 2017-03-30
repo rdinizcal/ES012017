ca2a4382eb83108744d7f26ed0f798872a374ad4
Hugo Fagundes <hugofagundes07@gmail.com>
2017-03-29 22:20:14 -0300

    share meta

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ES 01 2017' });
 });
 
 module.exports = router;
