68d152f01ea22318384b95019e5762c8091a415d
Hugo Fagundes <hugofagundes07@gmail.com>
2017-04-12 20:56:48 -0300

    .

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('users/login', { title: 'LOGIN' });
+
+  user = new Object();
+  user.name = req.query.name;
+  user.email = req.query.email;
+});
+
+router.post('/elogin', function(req, res, next) {
+	res.send(req.query.email)	
+})
+
 module.exports = router;
