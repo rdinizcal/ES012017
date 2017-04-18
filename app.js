4aea5522ece620dac60a4c37cfe9c79e5f6faf6d
Lucas Mafra Chagas <lucas.mafra@akuntsu.com.br>
2017-04-17 22:09:04 -0300

    Adicionando sass, bootstrap e fazendo funcionar angular

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
 app.use('/', index);
 app.use('/users', users);
 
